e97bb90587021434390cd921609c3a07d1c3c8e8
Fukuball Lin <fukuball@gmail.com>
2016-02-01 01:10:58 +0800

    refine two sum comment

--- Q1/q1-two-sum.js
+++ Q1/q1-two-sum.js
@@ -1,20 +1,5 @@
 /**
- * Given an array of integers, find two numbers such that they add up to a specific target number.
- *
- * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
- *
- * You may assume that each input would have exactly one solution.
- *
- * Input: numbers={2, 7, 11, 15}, target=9
- * Output: index1=1, index2=2
- *
- * O(n2) runtime, O(1) space Brute force:
- *
- * The brute force approach is simple. Loop through each element x and find if there is another value that equals to target x. As finding another value requires looping through the rest of array, its runtime complexity is O(n2).
- *
- * O(n) runtime, O(n) space Hash table:
- *
- * We could reduce the runtime complexity of looking up a value to O(1) using a hash map that maps a value to its index.
+ * q1-two-sum.js
  *
  * @param array nums
  * @param int target
@@ -42,5 +27,7 @@ var twoSum = function(nums, target) {
     return [];
 
 };
+
+//------------------------------ Simple Testing Code ------------------------------//
 
 print(twoSum([2, 7, 11, 15], 9));
--- Q1/q1-two-sum.php
+++ Q1/q1-two-sum.php
@@ -1,21 +1,6 @@
 <?php
 /**
- * Given an array of integers, find two numbers such that they add up to a specific target number.
- *
- * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
- *
- * You may assume that each input would have exactly one solution.
- *
- * Input: numbers={2, 7, 11, 15}, target=9
- * Output: index1=1, index2=2
- *
- * O(n2) runtime, O(1) space Brute force:
- *
- * The brute force approach is simple. Loop through each element x and find if there is another value that equals to target x. As finding another value requires looping through the rest of array, its runtime complexity is O(n2).
- *
- * O(n) runtime, O(n) space Hash table:
- *
- * We could reduce the runtime complexity of looking up a value to O(1) using a hash map that maps a value to its index.
+ * q1-two-sum.php
  *
  * @param array nums
  * @param int target
@@ -44,4 +29,6 @@ function two_sum($nums, $target) {
 
 }
 
-print_r(two_sum(array(2, 7, 11, 15), 9));
+//------------------------------ Simple Testing Code ------------------------------//
+
+var_dump(two_sum(array(2, 7, 11, 15), 9));
